ced10cc909f3a94bd6982136f9a0ea71ce243cf4
SH Hridoy <shhridoy.cste@gmail.com>
2017-01-24 21:46:45 +0600

    GUI - Popup Dialog box

--- GUI.java
+++ GUI.java
@@ -13,11 +13,11 @@ public class GUI {
         String fn = JOptionPane.showInputDialog("Enter First Number"); 
         String sn = JOptionPane.showInputDialog("Enter Second Number");
         
-        int num1 = Integer.parseInt(fn);
+        int num1 = Integer.parseInt(fn); //string to integer
         int num2 = Integer.parseInt(sn);
         int sum = num1 + num2;
         
-        JOptionPane.showMessageDialog(null, "The answer is "+sum, "The title", JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(null, "The answer is "+sum, "The title", JOptionPane.PLAIN_MESSAGE); //Show popup dialogue
         
     }
 }
