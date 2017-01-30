0c289ea4bcc015213bb805d49682e0f4a3f0b46b
SH Hridoy <shhridoy.cste@gmail.com>
2017-01-30 21:37:27 +0600

    Inharitance (Sub Class)

--- InheritanceClass.java
+++ InheritanceClass.java
@@ -3,7 +3,7 @@ package learn;
 
 public class InheritanceClass extends InheritanceClassCommon{
     
-    //override the InheritanceClassCommon eat() method
+    //override the InheritanceClassCommon eat()  method
     public void eat(){
         System.out.println("I am the new eat method of Inheritance Class.");
     }
