74769ca11b419c632818be8036fcc3c869b4a3cb
SH Hridoy <shhridoy.cste@gmail.com>
2017-01-30 22:21:00 +0600

    Polymorphism Overriding a Method

--- Polymorphism.java
+++ Polymorphism.java
@@ -1,6 +1,6 @@
 /** Polymorphism is the ability of an object to take on many forms. 
  * The most common use of polymorphism in OOP occurs when a parent class reference is used to refer to a child class object. 
- * Any Java object that can pass more than one IS-A test is considered to be polymorphic. **/
+ * Any Java object that can pass more than one IS-A test is considered to be polymorphic.  **/
 
 package learn;
 
