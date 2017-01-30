d050a4cbed0bc45e33c9d7c6eb97615876a55737
SH Hridoy <shhridoy.cste@gmail.com>
2017-01-30 22:25:53 +0600

    Polymorphism Overriding Method and Passing object

--- Polymorphism2Class.java
+++ Polymorphism2Class.java
@@ -3,7 +3,7 @@ package learn;
 public class Polymorphism2Class extends Polymorphism2ClassFood{
     
     public void eat(){
-        System.out.println("This Class food is great."); //we will override this method
+        System.out.println("This Class food is great.");  //we will override this method
     }
     
 }
