b9492441ba5ffa22100f8d47307e704bbea71f0a
SH Hridoy <shhridoy.cste@gmail.com>
2017-03-12 22:23:27 +0600

    Check Box by java coding

---
+++ CheckBoxGUI.java
@@ -0,0 +1,13 @@
+package checkboxgui;
+import javax.swing.JFrame;
+
+public class CheckBoxGUI {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        CheckBoxGUIClass gui = new CheckBoxGUIClass();
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setSize(280, 200);
+        gui.setVisible(true);
+    }
+}
