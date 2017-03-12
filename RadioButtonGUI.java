85b41303410c3b1a499ff008ed43f727551349b8
SH Hridoy <shhridoy.cste@gmail.com>
2017-03-12 22:40:09 +0600

    Add files via upload

---
+++ RadioButtonGUI.java
@@ -0,0 +1,14 @@
+package radiobuttongui;
+
+import javax.swing.JFrame;
+
+public class RadioButtonGUI {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        RadioButtonGUIClass gui = new RadioButtonGUIClass();
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setSize(300, 200);
+        gui.setVisible(true);
+    }
+}
