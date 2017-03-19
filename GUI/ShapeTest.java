56b5f70552bf4c1d5f74e5f6b704007092e4ebf1
SH Hridoy <shhridoy.cste@gmail.com>
2017-03-19 20:58:51 +0600

    program to create a GUI of shape rectangle and oval

---
+++ GUI/ShapeTest.java
@@ -0,0 +1,22 @@
+package shapetest;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class ShapeTest {
+
+    public static void main(String[] args) {
+        // TODO code application logic here 
+        String input = JOptionPane.showInputDialog("Enter 1 to draw rectangles\n"+"Enter 2 to draw ovals");
+        
+        int choice = Integer.parseInt(input);
+       
+        Shapes panel = new Shapes(choice);
+        
+        JFrame application = new JFrame();
+        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        application.add(panel);
+        application.setSize(300, 300);
+        application.setVisible(true);
+    }
+}
