53576411a3753467afd1cfdb0dce6ba27d2ab5e2
SH Hridoy <shhridoy.cste@gmail.com>
2017-03-25 21:52:34 +0600

    passing array reference to a method

--- PassArray.java
+++ PassArray.java
@@ -18,7 +18,7 @@ public class PassArray {
         System.out.printf("\nEffects of passing array element value:\narray[3] before modifyElement: %d\n", array[3]);
         
         modifyElement(array[3]);
-        System.out.printf("array[3] after modifyElement: %d\n", array[3]);
+        System.out.printf("\narray[3] after modifyElement: %d\n", array[3]);
     }
     
     public static void modifyArray(int[] arr){
@@ -32,3 +32,13 @@ public class PassArray {
         System.out.printf("Value of element in modifyElement: %d\n", element);
     }
 }
+
+/* OUTPUT: 
+The values of the original array are: 1 2 3 4 5 
+
+The values of the modified array are: 2 4 6 8 10
+
+array[3] before modifyElement: 8
+Value of element in modifyElement: 16
+array[3] after modifyElement: 8
+*/
