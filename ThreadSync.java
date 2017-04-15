c5e0179756b4d4d83d7741f2e78947402c49f8ef
SH Hridoy <shhridoy.cste@gmail.com>
2017-04-15 21:01:25 +0600

    using synchronized in thread

---
+++ ThreadSync.java
@@ -0,0 +1,60 @@
+package threaddemo;
+
+class Callme {   
+    
+    synchronized void call(String msg){
+        System.out.print("["+msg);
+        try{
+            Thread.sleep(1000);
+        }catch(InterruptedException e){
+            System.out.println("Interrupted");
+        }
+        System.out.println("]");
+    }
+    
+}
+
+
+class Caller implements Runnable{
+    String msg;
+    Callme target;
+    Thread t;
+    
+    public Caller(Callme targ, String s){
+        target = targ;
+        msg = s;
+        t = new Thread(this);
+        t.start();
+    }
+
+    @Override
+    public void run() {
+        target.call(msg);
+    }
+    
+}
+
+public class ThreadSync {
+
+    public static void main(String[] args) {
+        Callme target = new Callme();
+        Caller ob1 = new Caller(target, "Hello");
+        Caller ob2 = new Caller(target, "synchronized");
+        Caller ob3 = new Caller(target, "World");
+        
+        // wait for threads to end
+        try{
+            ob1.t.join();
+            ob2.t.join();
+            ob3.t.join();
+        } catch(InterruptedException e){
+            System.out.println("Interrupted");
+        }
+    }
+}
+
+/** OUTPUT:
+[Hello]
+[synchronized]
+[World]
+ */
